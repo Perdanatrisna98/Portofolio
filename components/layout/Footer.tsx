7dcfbd7f17f401ccca1f9687e59ca6a195b3c752
Erlangga Perdana <erlanggatrisna791@gmail.com>
2026-07-01 12:04:27 +0700

    feat(layout): add footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,20 @@
+import Container from "./container";
+
+export default function Footer() {
+    return (
+        <footer className="py-10">
+
+            <Container>
+                <p className="text-center text-sm opacity-70">
+                    &copy; {new Date().getFullYear()}
+
+                    {" "}
+
+                    Erlangga Trisna Yudha Perdana
+                </p>
+
+            </Container>
+
+        </footer>
+    )
+}
