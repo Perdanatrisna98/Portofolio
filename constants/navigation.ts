94cfb7c8dfe790f205ef7636c35072651a2b34ec
Erlangga Perdana <erlanggatrisna791@gmail.com>
2026-07-01 11:18:40 +0700

    chore: setup project architecture and design system foundation

---
+++ constants/navigation.ts
@@ -0,0 +1,22 @@
+export const navigation = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "About",
+        href: "#about",
+    },
+    {
+        title: "Projects",
+        href: "#projects",
+    },
+    {
+        title: "Experience",
+        href: "#experience",
+    },
+    {
+        title: "Contact",
+        href: "#contact",
+    },
+]
---
+++ constants/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+    name: "Erlangga",
+    title: "Erlangga Trisna Yudha Perdana",
+    description: "SMK Student • Laravel Developer • Learning React",
+    url: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    email: "",
+};
